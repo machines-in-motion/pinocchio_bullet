48285a4ae92d430a78f2e2c4b4c326c5a77cf136
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2020-11-03 14:43:12 +0100

    fix some file header

--- src/pinocchio_bullet/wrapper.py
+++ src/pinocchio_bullet/wrapper.py
@@ -1,11 +1,11 @@
 """wrapper
 
+Pybullet interface using pinocchio's convention.
+
 License: BSD 3-Clause License
 Copyright (C) 2018-2019, New York University , Max Planck Gesellschaft
 Copyright note valid unless otherwise stated in individual files.
 All rights reserved.
-
-Pybullet interface using pinocchio's convention.
 """
 
 import pybullet
